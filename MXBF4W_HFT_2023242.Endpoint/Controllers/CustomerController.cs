940349eb5e845148720a12f64d07f4b1e42d0006
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-05-04 12:48:24 +0200

    CustomerController is done

--- MXBF4W_HFT_2023242.Endpoint/Controllers/CustomerController.cs
+++ MXBF4W_HFT_2023242.Endpoint/Controllers/CustomerController.cs
@@ -1,6 +1,55 @@
-﻿namespace MXBF4W_HFT_2023242.Endpoint.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MXBF4W_HFT_2023242.Logic;
+using MXBF4W_HFT_2023242.Models;
+using System.Collections.Generic;
+
+namespace MXBF4W_HFT_2023242.Endpoint.Controllers
 {
-    public class CustomerController
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
     {
+
+        ICustomerLogic logic;
+
+        public CustomerController(ICustomerLogic logic)
+        {
+            this.logic = logic;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Customer> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+
+        [HttpGet("{id}")]
+        public Customer Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+
+        [HttpPost]
+        public void Create([FromBody] Customer value)
+        {
+            this.logic.Create(value);
+        }
+
+
+        [HttpPut]
+        public void Update([FromBody] Customer value)
+        {
+            this.logic.Update(value);
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
