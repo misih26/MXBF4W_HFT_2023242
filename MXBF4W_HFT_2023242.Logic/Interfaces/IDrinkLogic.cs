7da841115890de391494c04f5ec8dd8597d45d59
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-05-04 11:26:12 +0200

    New class

---
+++ MXBF4W_HFT_2023242.Logic/Interfaces/IDrinkLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MXBF4W_HFT_2023242.Logic.Interfaces
+{
+    internal class IDrinkLogic
+    {
+    }
+}
