dd79384e9e1d66804530234f95962bea3253e251
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-05-04 12:56:26 +0200

    DrinkControllers is done

--- MXBF4W_HFT_2023242.Endpoint/Controllers/DrinkController.cs
+++ MXBF4W_HFT_2023242.Endpoint/Controllers/DrinkController.cs
@@ -1,6 +1,54 @@
-﻿namespace MXBF4W_HFT_2023242.Endpoint.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MXBF4W_HFT_2023242.Logic;
+using MXBF4W_HFT_2023242.Models;
+using System.Collections.Generic;
+
+namespace MXBF4W_HFT_2023242.Endpoint.Controllers
 {
-    public class DrinkController
+    [Route("[controller]")]
+    [ApiController]
+    public class DrinkController : ControllerBase
     {
+        IDrinkLogic logic;
+
+        public DrinkController(IDrinkLogic logic)
+        {
+            this.logic = logic;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Drink> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+
+        [HttpGet("{id}")]
+        public Drink Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+
+        [HttpPost]
+        public void Create([FromBody] Drink value)
+        {
+            this.logic.Create(value);
+        }
+
+
+        [HttpPut]
+        public void Update([FromBody] Drink value)
+        {
+            this.logic.Update(value);
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
