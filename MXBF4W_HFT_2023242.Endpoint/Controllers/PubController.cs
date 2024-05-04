7cb56c57bf05feab5ee722b013775218c350d9d5
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-05-04 12:57:02 +0200

    PubController is done

--- MXBF4W_HFT_2023242.Endpoint/Controllers/PubController.cs
+++ MXBF4W_HFT_2023242.Endpoint/Controllers/PubController.cs
@@ -1,6 +1,54 @@
-﻿namespace MXBF4W_HFT_2023242.Endpoint.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MXBF4W_HFT_2023242.Logic;
+using MXBF4W_HFT_2023242.Models;
+using System.Collections.Generic;
+
+namespace MXBF4W_HFT_2023242.Endpoint.Controllers
 {
-    public class PubController
+    [Route("[controller]")]
+    [ApiController]
+    public class PubController : ControllerBase
     {
+        IPubLogic logic;
+
+        public PubController(IPubLogic logic)
+        {
+            this.logic = logic;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Pub> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+
+        [HttpGet("{id}")]
+        public Pub Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+
+        [HttpPost]
+        public void Create([FromBody] Pub value)
+        {
+            this.logic.Create(value);
+        }
+
+
+        [HttpPut]
+        public void Update([FromBody] Pub value)
+        {
+            this.logic.Update(value);
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
